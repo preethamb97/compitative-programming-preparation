009b84c37fb23fcfcbe33e8070d68d91118a6e0c
Preetham B <preethamb97@gmail.com>
2022-03-11 22:44:17 +0530

    added a.cpp basic techniques

---
+++ BASIC TECHNIQUES/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b;
+    string x;
+    cin >> a >> b >> x;
+    cout << "\n a is :" << a << "\n b is :" << b << "\n x is :" << x << "\n";
+
+    // alternate solution 
+    // int c, d;
+    // scanf("%d %d", &c, &d);
+    // printf("\n c is :%d d is :%d\n", c, d);
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # compitative-programming-preparation
 c++ as base language
+
+
+
+# To run the code
+## g++ -std=c++11 -02 -Wall a.cpp -o test
+### example g++ -std=c++11 -O2 -Wall a.cpp -o test && ./test
+
+# Folders and Its contents
+
+# BASIC TECHNIQUES
+
+## a -> basic cpp syntax
